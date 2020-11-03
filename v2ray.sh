b5a8be3d25c8f82a8d172e4f6b6ad7d7875b44e3
FFlowerS <i@fflowers.moe>
2020-11-03 11:05:00 +0800

    1

--- v2ray.sh
+++ v2ray.sh
@@ -64,8 +64,3 @@ echo "Prepare to use"
 unzip v2ray.zip && chmod +x v2ray v2ctl
 mv v2ray v2ctl /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
-mv config.json /etc/v2ray/config.json
-
-# Clean
-rm -rf ${PWD}/*
-echo "Done"
